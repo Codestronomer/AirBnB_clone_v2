1d887626649774b7a9c307c054bf0c03b08a40c5
codestronomer <johnrumide6@gmail.com>
2022-11-13 23:37:32 +0100

    [fix] remove braces from url route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def c_text(text):
 
 
 @app.route("/python", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """Displays python followed by the value of the text"""
     return "Python {}".format(text.replace("_", " "))
