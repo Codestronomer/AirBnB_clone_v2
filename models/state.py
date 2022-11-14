cebaab2b853fbcf79a1c8de4327aad12a98d8e25
codestronomer <johnrumide6@gmail.com>
2022-11-14 03:01:07 +0100

    [update] change from file storage to db

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
+from os import getenv
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Return the list of City objects related to this State"""
+            name = ""
+            cities = [v for k, v in models.storage.all().items()
+                      if 'City' in k and v.state_id == self.id]
+            return cities
