aae2bf15160efb85b3c2aa5560740bf6d7041e18
codestronomer <johnrumide6@gmail.com>
2022-11-14 02:01:28 +0100

    [update] add new delete method to filestorage'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Create a new database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbtn_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON preformance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbtn_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `preformance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
