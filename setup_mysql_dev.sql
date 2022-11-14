f4e7556750413814e260d34fc363ddf77dc94c7b
codestronomer <johnrumide6@gmail.com>
2022-11-14 01:42:23 +0100

    [fix] sql statements

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- Create a new database and user
-CREATE DATABASE hbtn_dev_db IF NOT EXISTS;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd' IF NOT EXISTS;
-GRANT ALL PRIVILEGES ON hbtn_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON preformance_schema TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd' IF NOT EXISTS;
+GRANT ALL PRIVILEGES ON `hbtn_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `preformance_schema` TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
